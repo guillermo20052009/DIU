f3475a5f811b70513bd0ece89efc78a61fcd9e1d
guillermo <guillermo.barcenalopez@iesvalleinclan.es>
2024-09-30 21:03:27 +0200

    DobleContador

--- src/main/java/com/example/javafx/DobleContador.java
+++ src/main/java/com/example/javafx/DobleContador.java
@@ -1,28 +1,26 @@
 package com.example.javafx;
 
 import javafx.application.Application;
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.SimpleIntegerProperty;
 import javafx.stage.Stage;
 
 public class DobleContador extends Application {
 
     // Instancias de la clase Contador
-
     Contador contador1 = new Contador();
     Contador contador2 = new Contador();
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         // Crear y mostrar el primer contador
-
         Stage stage1 = new Stage();
         contador1.start(stage1);
 
         // Crear y mostrar el segundo contador
         Stage stage2 = new Stage();
         contador2.start(stage2);
-        //contador1.numero.textProperty().bindBidirectional(contador2.numero.textProperty());
 
+        // Vincular las propiedades de numeroPuls de ambos contadores bidireccionalmente
+        contador1.numeroPulsProperty().bindBidirectional(contador2.numeroPulsProperty());
     }
 
     public static void main(String[] args) {
